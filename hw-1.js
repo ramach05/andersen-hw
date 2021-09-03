42c035a6cdf5463a41caae694fcc1fede1252201
Roman <mrroman6552@gmail.com>
2021-09-03 17:27:58 +0300

    refactor: hw-1

--- hw-1.js
+++ hw-1.js
@@ -1,42 +1,37 @@
-// // -------------------- 1 задача --------------------
+// -------------------- 1 задача --------------------
 
-// const firstValue = prompt("Введите число");
-// const secondValue = prompt("Введите систему счисления");
+const firstValue = prompt("Введите число");
+const secondValue = prompt("Введите систему счисления");
 
-// function convertNumber(a, b) {
-//   const number = Number(a);
-//   const numberSystem = Number(b);
+function convertNumber(a, b) {
+  const number = Number(a);
+  const numberSystem = Number(b);
 
-//   if (isNaN(number) || isNaN(numberSystem)) {
-//     return console.log("Некорректный ввод!");
-//   }
-//   return number.toString(numberSystem);
-// }
+  if (isNaN(number) || isNaN(numberSystem)) {
+    return console.log("Некорректный ввод!");
+  }
+  return number.toString(numberSystem);
+}
 
-// console.log(convertNumber(firstValue, secondValue));
+console.log(convertNumber(firstValue, secondValue));
 
 // -------------------- 2 задача --------------------
 
 const thirdValue = prompt("Введите первое число");
 const number1 = Number(thirdValue);
 
-switch (isNaN(number1)) {
-  case true:
-    console.log("Некорректный ввод!");
-    break;
+if (isNaN(number1)) {
+  console.log("Некорректный ввод!");
+} else {
+  const fourthValue = prompt("Введите второе число");
+  const number2 = Number(fourthValue);
 
-  default:
-    const fourthValue = prompt("Введите второе число");
-    const number2 = Number(fourthValue);
-
-    if (isNaN(number2)) {
-      console.log("Некорректный ввод!");
-      break;
-    }
-
-    console.log(makeOperations(thirdValue, fourthValue));
+  console.log(makeOperations(number1, number2));
 }
 
 function makeOperations(a, b) {
+  if (isNaN(a) || isNaN(b)) {
+    return console.log("Некорректный ввод!");
+  }
   return `Ответ: ${a + b}, ${a / b}`;
 }
