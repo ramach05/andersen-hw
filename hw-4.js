4baf383b1393b968463b8fcc129ac941a8c5316a
Roman <mrroman6552@gmail.com>
2021-09-13 17:20:40 +0300

    feat: make hw-4.2

--- hw-4.js
+++ hw-4.js
@@ -19,9 +19,7 @@
 
 function concatStrings(firstWord, separator) {
   return function (nextWord) {
-    if (typeof nextWord !== "string") {
-      return firstWord;
-    }
+    if (typeof nextWord !== "string") return firstWord;
 
     if (typeof separator !== "string") {
       const newStr = firstWord + nextWord;
@@ -42,3 +40,65 @@ function concatStrings(firstWord, separator) {
 // console.log(concatStrings("some-value")("")("")(null), "some-value");
 // console.log(concatStrings("some-value")(2), "some-value");
 // console.log(concatStrings("some-value")("333")(123n), "some-val333");
+
+// 2) Создать класс Calculator. Конструктор класса должен принимать два валидных числа, иначе (если параметра не два или хотя бы один из них невалидный number) бросать ошибку (throw new Error('')). Данный класс должен иметь методы setX, setY, logSum, logMul, logSub, logDiv.
+// - setX(num) - задаёт первому из чисел новое значение. Кидать ошибку если параметр не передан или является невалидным числом;
+// - setY(num) - задаёт второму из чисел новое значение. Кидать ошибку если параметр не передан или является невалидным числом;
+// - logSum() - выводит в консоль сумму двух наших чисел внутри класса;
+// - logMul() - выводит в консоль произведение двух наших чисел внутри класса;
+// - logSub() - выводит в консоль разность двух наших чисел внутри класса;
+// - logDiv() - выводит в консоль частное двух наших чисел внутри класса ИЛИ кидает ошибку, если второе число (Y) равняется нулю.
+// !ВАЖНО! Все методы класса должны отрабатывать корректно ДАЖЕ в случае копирования функций в отдельные переменные.
+
+class Calculator {
+  constructor(x, y) {
+    if (
+      typeof x !== "number" ||
+      typeof y !== "number" ||
+      !isFinite(x) ||
+      !isFinite(y)
+    ) {
+      throw new Error("Ошибка!");
+    }
+    this.x = x;
+    this.y = y;
+  }
+
+  logSum = () => {
+    console.log(this.x + this.y);
+  };
+  logMul = () => {
+    console.log(this.x * this.y);
+  };
+  logSub = () => {
+    console.log(this.x - this.y);
+  };
+  logDiv = () => {
+    if (this.y === 0) {
+      throw new Error("Ошибка!");
+    }
+    console.log(this.x / this.y);
+  };
+  setX = (num) => {
+    if (typeof num !== "number" || !isFinite(num)) {
+      throw new Error("Ошибка!");
+    }
+    this.x = num;
+  };
+  setY = (num) => {
+    if (typeof num !== "number" || !isFinite(num)) {
+      throw new Error("Ошибка!");
+    }
+    this.y = num;
+  };
+}
+
+const calculator = new Calculator(12, 3);
+calculator.logSum(); // 15
+calculator.logDiv(); // 4
+calculator.setX(15);
+calculator.logDiv(); // 5
+
+const logCalculatorDiv = calculator.logDiv;
+logCalculatorDiv(); // 5
+calculator.setY(444n); // Ошибка!
